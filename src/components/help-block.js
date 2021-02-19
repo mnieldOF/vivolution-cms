78be40131cb640076fa64fb55d25083ae0d8d292
Michael Nield <mnield@openfrequency.com>
2021-02-19 13:57:57 +0000

    partners responsive styles

--- src/components/help-block.js
+++ src/components/help-block.js
@@ -17,74 +17,74 @@ const HelpBlock = () => {
             </picture>
             <div className="container column">
                 <h2 className='title'>Who we help</h2>
+                    <div className="grid">
+                        <Swiper id='main' slidesPerView={2}  
+                            breakpoints={{
 
-                    <Swiper id='main' slidesPerView={2}  
-                        breakpoints={{
+                                320: {
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 20,
+                                },
+                                // when window width is >= 480px
+                                480: {
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 30
+                                },
+                                // when window width is >= 640px
+                                640: {
+                                    slidesPerView: 4,
+                                    touchStartForcePreventDefault: true,
+                                    spaceBetween: 40,
+                                    allowTouchMove: false,
+                                    shortSwipes: false,
+                                }
+                            }}
 
-                            320: {
-                                slidesPerView: 'auto',
-                                spaceBetween: 20,
-                              },
-                              // when window width is >= 480px
-                              480: {
-                                slidesPerView: 'auto',
-                                spaceBetween: 30
-                              },
-                              // when window width is >= 640px
-                              640: {
-                                slidesPerView: 4,
-                                touchStartForcePreventDefault: true,
-                                spaceBetween: 40,
-                                allowTouchMove: false,
-                                shortSwipes: false,
-                              }
-                        }}
-
-                    > 
-                        <SwiperSlide className="help-card">
-                            <picture>
-                                <source media="(min-width: 0px)"  
-                                        srcset={imgMob}
-                                />
-                                <img className='card-img' src={imgMob} />
-                            </picture>
-                            <div className="content">
-                                <Icon icon='scaleup-Icon' size='65'/>
-                                <h4 className='c-title'>Start up / Scale up</h4>
-                                <p>Investing in companies that we believe will make a positive difference. Our team will super-charge your business by providing the expertise you need to get to the next level.</p>
-                                <a href=""><Icon icon='right' size='30'/>How we support start ups/ scale ups</a>
-                            </div>                         
-                        </SwiperSlide>
-                        <SwiperSlide className="help-card">
-                            <picture>
-                                <source media="(min-width: 0px)"  
-                                        srcset={imgMob}
-                                />
-                                <img className='card-img' src={imgMob} />
-                            </picture>   
-                            <div className="content">
-                                <Icon icon='arrows' size='65'/>
-                                <h4 className='c-title'>Investors</h4>
-                                <p>Investing in companies that we believe will make a positive difference. Our team will super-charge your business by providing the expertise you need to get to the next level.</p>
-                                <a href=""><Icon icon='right' size='30'/>How we support Investors</a>
-                            </div>                      
-                        </SwiperSlide>
-                        <SwiperSlide className="help-card">
-                            <picture>
-                                <source media="(min-width: 0px)"  
-                                        srcset={imgMob}
-                                />
-                                <img className='card-img' src={imgMob} />
-                            </picture>      
-                            <div className="content">
-                                <Icon icon='people' size='65'/>
-                                <h4 className='c-title'>Corporate</h4>
-                                <p>Identifying opportunities for potential Ventures. Through our portfolio of companies and collaborative projects, we have our finger on the pulse of the latest emerging tech.</p>
-                                <a href=""><Icon icon='right' size='30'/>How we support Corporates</a>
-                            </div>                   
-                        </SwiperSlide>
-                    </Swiper>
-
+                        > 
+                            <SwiperSlide className="help-card">
+                                <picture>
+                                    <source media="(min-width: 0px)"  
+                                            srcset={imgMob}
+                                    />
+                                    <img className='card-img' src={imgMob} />
+                                </picture>
+                                <div className="content">
+                                    <Icon icon='scaleup-Icon' size='65'/>
+                                    <h4 className='c-title'>Start up / Scale up</h4>
+                                    <p>Investing in companies that we believe will make a positive difference. Our team will super-charge your business by providing the expertise you need to get to the next level.</p>
+                                    <a href=""><Icon icon='right' size='30'/>How we support start ups/ scale ups</a>
+                                </div>                         
+                            </SwiperSlide>
+                            <SwiperSlide className="help-card">
+                                <picture>
+                                    <source media="(min-width: 0px)"  
+                                            srcset={imgMob}
+                                    />
+                                    <img className='card-img' src={imgMob} />
+                                </picture>   
+                                <div className="content">
+                                    <Icon icon='arrows' size='65'/>
+                                    <h4 className='c-title'>Investors</h4>
+                                    <p>Investing in companies that we believe will make a positive difference. Our team will super-charge your business by providing the expertise you need to get to the next level.</p>
+                                    <a href=""><Icon icon='right' size='30'/>How we support Investors</a>
+                                </div>                      
+                            </SwiperSlide>
+                            <SwiperSlide className="help-card">
+                                <picture>
+                                    <source media="(min-width: 0px)"  
+                                            srcset={imgMob}
+                                    />
+                                    <img className='card-img' src={imgMob} />
+                                </picture>      
+                                <div className="content">
+                                    <Icon icon='people' size='65'/>
+                                    <h4 className='c-title'>Corporate</h4>
+                                    <p>Identifying opportunities for potential Ventures. Through our portfolio of companies and collaborative projects, we have our finger on the pulse of the latest emerging tech.</p>
+                                    <a href=""><Icon icon='right' size='30'/>How we support Corporates</a>
+                                </div>                   
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
             </div>
         </div>
     )
