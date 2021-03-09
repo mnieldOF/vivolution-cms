27d4e65a7ec9d6e281a4b4bfdc5e5f90fad27378
Michael Nield <mnield@openfrequency.com>
2021-03-09 19:39:19 +0000

    home page tweaks

--- src/components/text-block.js
+++ src/components/text-block.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TextBlock = ({title, text}) => {
     return(
         <div className='text-block'>
-            <div className='container'>
+            <div className='content-container'>
                 <div className="box">
                     <h2 className='title'>{title}</h2>
                     <p className='text'>{text}</p>
