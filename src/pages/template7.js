df56501c0fba1f69a8963b30b81265fd004e15ae
Michael Nield <mnield@openfrequency.com>
2021-03-09 09:32:03 +0000

    template 7 progress

--- src/pages/template7.js
+++ src/pages/template7.js
@@ -12,6 +12,25 @@ const TemplateSeven = () => {
             <Hero />
             <QuoteBlock />
             <Cards />
+            <section className="case-about">
+                <div className="content-container column">
+                    <div className="meta">
+                        <h5 className="section-title">About</h5>  
+                        <h2 className="title">Oxford Cancer Biomarkers</h2>
+                    </div>    
+                    <div className="grid">
+                        <div>
+                            <p>Oxford Cancer Biomarkers (OCB) provides personalised solutions that meet important needs across the cancer care pathway. Early detection of cancer risk and dramatically improving outcomes using personalised medicine and digital healthcare are core themes.  OCB achieves these aims by applying platforms that incorporate biomarker-based technology with digital solutions and integrated lifestyle advice.</p>
+                        </div>
+                        <div>
+                            <p>OCB is a partner to the University Hospital Coventry and Warwickshire NHS Trust’s successful bid for the PathLAKE project; in which UK Research and Innovation will invest £10million as part of the Industrial Strategy Challenge Fund. OCB’s role is to provide cutting-edge expertise through its existing ColoProg® image analysis platform. The PathLAKE </p>
+                        </div>
+                        <div>
+                            <p>resources will enable OCB and its IBM channel partner Meridian IT to identify novel diagnostic biomarkers in the tumour microenvironment to improve early diagnosis and individual patient risk stratification. Oxford Cancer Biomarkers is closely affiliated with The University of Oxford, where its founders lead research groups focused on translational cancer science.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Layout>
     )
 }
