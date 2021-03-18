54f2eb9e2bc28bfd9bf0db42180b36391cd3a660
Michael Nield <mnield@openfrequency.com>
2021-03-18 12:28:45 +0000

    home page hero cms added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from "../components/layout"
-import ImageText from "../components/image-text"
-import Hero from "../components/hero"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import ImageText from "../components/image-text";
+import Hero from "../components/hero";
 import TextBlock from "../components/text-block";
 import Partners from "../components/partners";
 import HelpBlock from "../components/help-block";
@@ -10,22 +10,29 @@ import GrowthBlock from "../components/growth-block";
 import Testimonial from "../components/testimonial";
 import ImageGallery from "../components/image-gallery";
 import Slider from "../components/slider";
-import ContentReveal from "../components/content-reveal"
+import ContentReveal from "../components/content-reveal";
 import SubFooter from "../components/sub-footer";
 import Footer from "../components/footer";
 
-const IndexPage = ({ data }) => (
-  <Layout> 
-    <Hero title='Building ventures that make a difference' />
-    <TextBlock 
-      title='We give our clients the team they need, when they need it most - at the start of the journey.'  
-      text='From Devs to Directors, bringing you a wealth of experience and expertise to build your business. On your terms. Invested in your business and there for the long term, no matter how bumpy the ride.'
-    />
-    <HelpBlock />
-    <GrowthBlock />
-    <Testimonial />
-    <Partners />
-    <Slider />
+const IndexPage = ({ data }) => {
+  const { datoCmsHome } = data;
+  console.log(datoCmsHome);
+
+  return (
+    <Layout>
+      <Hero
+        title={datoCmsHome.content[0].title}
+        image={datoCmsHome.content[0].background}
+      />
+      <TextBlock
+        title="We give our clients the team they need, when they need it most - at the start of the journey."
+        text="From Devs to Directors, bringing you a wealth of experience and expertise to build your business. On your terms. Invested in your business and there for the long term, no matter how bumpy the ride."
+      />
+      <HelpBlock />
+      <GrowthBlock />
+      <Testimonial />
+      <Partners />
+      <Slider />
       {/* <Masonry className="showcase">
         {data.allDatoCmsWork.edges.map(({ node: work }) => (
           <div key={work.id} className="showcase__item">
@@ -45,27 +52,24 @@ const IndexPage = ({ data }) => (
           </div>
         ))}
       </Masonry> */}
-  </Layout>
-)
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
-  query IndexQuery {
-    allDatoCmsWork(sort: { fields: [position], order: ASC }) {
-      edges {
-        node {
+  {
+    datoCmsHome {
+      content {
+        ... on DatoCmsHeroBanner {
           id
-          title
-          slug
-          excerpt
-          coverImage {
-            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
-              ...GatsbyDatoCmsSizes
-            }
+          background {
+            gatsbyImageData
           }
+          title
         }
       }
     }
   }
-`
+`;
