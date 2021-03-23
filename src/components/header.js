e2cb317a61e106040e9325ed09bd575e7860150a
Michael Nield <mnield@openfrequency.com>
2021-03-23 13:38:00 +0000

    contact link

--- src/components/header.js
+++ src/components/header.js
@@ -1,52 +1,52 @@
 import React, { useState } from "react";
-import Icon from './icon';
-import CoverMenu from './cover-menu';
-import { motion } from "framer-motion"
-
+import Icon from "./icon";
+import CoverMenu from "./cover-menu";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  const [active, setActive] = useState(false);
 
-    const [ active, setActive ] = useState(false);
+  const openMenu = (e) => {
+    e.preventDefault();
+    setActive(!active);
+  };
 
-
-    const openMenu = (e) => {
-        e.preventDefault();
-        setActive(!active);
-    }
-
-    return(
-        <>
-            <div className='header'>
-                    <div className="container">
-                        <div className={`logo-container ${active ? 'active' : ''}`}>
-                            <Icon icon='vivo-logo' width='100px' className='logo'/>
-                            <Icon icon='v-logo' size='30px' className='logo-small'/>
-                        </div>
-                        <nav>
-                            <div>
-                                <a href="" className={`chat-cta ${active ? 'active' : ''}`}>let's chat</a>
-                            </div>
-                            <motion.div className={`hamburger ${active ? 'active' : ''}`}>
-                                <div className="hamburger-wrapper">
-                                    <div className="button">
-                                        <button>
-                                            <div className="background-hamburger"></div>
-                                            <div className="icon-hamburger">
-                                                <div className="line-center"></div>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="click-layer"
-                                    onClick={(e) => openMenu(e)}
-                                ></div>
-                            </motion.div>
-                        </nav>  
-                </div>
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <div className={`logo-container ${active ? "active" : ""}`}>
+            <Icon icon="vivo-logo" width="100px" className="logo" />
+            <Icon icon="v-logo" size="30px" className="logo-small" />
+          </div>
+          <nav>
+            <div>
+              <a
+                href="/contact"
+                className={`chat-cta ${active ? "active" : ""}`}
+              >
+                let's chat
+              </a>
             </div>
-            <CoverMenu active={active} />
-        </>
-    )
-}
+            <motion.div className={`hamburger ${active ? "active" : ""}`}>
+              <div className="hamburger-wrapper">
+                <div className="button">
+                  <button>
+                    <div className="background-hamburger"></div>
+                    <div className="icon-hamburger">
+                      <div className="line-center"></div>
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <div className="click-layer" onClick={(e) => openMenu(e)}></div>
+            </motion.div>
+          </nav>
+        </div>
+      </div>
+      <CoverMenu active={active} />
+    </>
+  );
+};
 
 export default Header;
