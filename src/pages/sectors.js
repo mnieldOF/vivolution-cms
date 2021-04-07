0c37a0e8e10fc06ae31c635fb9a630ffaea5872f
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-04-07 17:00:52 +0100

    sectors template

--- src/pages/sectors.js
+++ src/pages/sectors.js
@@ -1,19 +1,44 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout.js";
 import Hero from "../components/hero";
 import ImageText from "../components/image-text";
 import ContentReveal from "../components/content-reveal";
 import Partners from "../components/partners";
 
-const Sectors = () => {
+const Sectors = ({ data }) => {
+  console.log(data);
   return (
     <Layout>
-      <Hero />
+      <Hero
+        image={data.datoCmsSectorSingle.hero[0].background}
+        title={data.datoCmsSectorSingle.hero[0].title}
+      />
       <ImageText />
-      <ContentReveal />
+      <ContentReveal tabs={data.datoCmsSectorSingle.tabs} />
       <Partners />
     </Layout>
   );
 };
 
 export default Sectors;
+
+export const query = graphql`
+  {
+    datoCmsSectorSingle {
+      tabs {
+        title
+        tabContent {
+          value
+        }
+      }
+      title
+      hero {
+        background {
+          gatsbyImageData
+        }
+        title
+      }
+    }
+  }
+`;
