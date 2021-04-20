e2142b2337dfa922b2c8d470b9f29a5551f78d43
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-04-20 15:47:05 +0100

    footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,35 +5,38 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="content-container">
-        <ul className="left-menu">
-          <li>
-            <a href="">Facebook</a>
-          </li>
-          <li>
-            <a href="">Linkedin</a>
-          </li>
-          <li>
-            <a href="">Instagram</a>
-          </li>
-          <li>
-            <a href="">Twitter</a>
-          </li>
-        </ul>
-        <Icon icon="vivo-logo" size="150px" />
-        <ul className="right-menu">
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Service</a>
-          </li>
-          <li>
-            <a href="">Contact us</a>
-          </li>
-        </ul>
+        <div className="flex">
+          <ul className="left-menu">
+            <li>
+              <a href="">Facebook</a>
+            </li>
+            <li>
+              <a href="">Linkedin</a>
+            </li>
+            <li>
+              <a href="">Instagram</a>
+            </li>
+            <li>
+              <a href="">Twitter</a>
+            </li>
+          </ul>
+          <Icon icon="vivo-logo" size="150px" className="logo" />
+          <Icon icon="v-logo" size="30px" className="logo-small" />
+          <ul className="right-menu">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Service</a>
+            </li>
+            <li>
+              <a href="">Contact us</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
