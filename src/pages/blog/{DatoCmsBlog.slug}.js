dbaad4f73ebd606b2fc48e528779f5b2ec97a514
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-09-02 14:27:43 +0100

    blog error fix

--- src/pages/blog/{DatoCmsBlog.slug}.js
+++ src/pages/blog/{DatoCmsBlog.slug}.js
@@ -1,1 +1,34 @@
 import * as React from "react";
+import { graphql } from "gatsby";
+
+const BlogPost = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <Hero
+        title={data.datoCmsSector.heroBanner[0].title}
+        image={data.datoCmsSector.heroBanner[0].background}
+      />
+      <ImageText
+        title={data.datoCmsSector.imageText[0].title}
+        text={data.datoCmsSector.imageText[0].subText}
+        image={data.datoCmsSector.imageText[0].image}
+      />
+      <ContentReveal
+        tabs={data.datoCmsSector.tabs}
+        tabTitle={data.datoCmsSector.tabTitle}
+      />
+      <Partners />
+    </Layout>
+  );
+};
+
+export default BlogPost;
+
+export const query = graphql`
+  query($id: String!) {
+    datoCmsBlog(id: { eq: $id }) {
+      id
+    }
+  }
+`;
