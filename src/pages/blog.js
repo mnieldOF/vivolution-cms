71936a0d29582e516106f31305959b854cc125d2
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-09-06 14:28:38 +0100

    blog landing page progress

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,109 @@
 import * as React from "react";
+import { graphql } from "gatsby";
+import styled from "@emotion/styled";
+import Layout from "../components/layout";
+import Hero from "../components/hero";
+import ProfileList from "../components/profile-list";
+import PortfolioFilter from "../components/portfolio-filter";
 
-const Blog = () => {
-  return <h1>Blog</h1>;
+const Grid = styled.div`
+  display: grid;
+  padding: 30px 0;
+  grid-gap: 20px;
+  @media screen and (min-width: 900px) {
+    padding: 100px 0;
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+const TextBlock = styled.div`
+  padding: 30px 0;
+  background: var(--color-primary-white);
+  max-width: 875px;
+  margin: 0 auto;
+  @media screen and (min-width: 900px) {
+    padding: 60px 0;
+  }
+`;
+
+const Section = styled.div`
+  background: var(--color-primary-white);
+  padding: 30px 0;
+  @media screen and (min-width: 900px) {
+    padding: 60px 0;
+  }
+`;
+
+const Blog = ({ data }) => {
+  console.log(data);
+  const [posts, setPosts] = React.useState(data.allDatoCmsBlog.edges);
+
+  const allCategories = [
+    ...new Set(posts.map((item) => item.node.category.category)),
+  ];
+
+  const [buttons, setButton] = React.useState(allCategories);
+
+  const filter = (button) => {
+    if (button === "All") {
+      setPosts(data.allDatoCmsBlog.edges);
+      return;
+    }
+    const filteredData = data.allDatoCmsBlog.edges.filter(
+      (item) => item.node.category.category === button
+    );
+    console.log(filteredData);
+    setPosts(filteredData);
+  };
+
+  const { hero, description } = data.datoCmsBlogLanding;
+  console.log(hero);
+  return (
+    <Layout>
+      <Hero title={hero[0].title} image={hero[0].background} />
+      <TextBlock>
+        <div className="content-container">{description}</div>
+      </TextBlock>
+      <Section>
+        <div className="content-container column">
+          <PortfolioFilter filter={filter} buttons={buttons} />
+          <Grid>
+            <ProfileList profiles={posts} />
+          </Grid>
+        </div>
+      </Section>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export const query = graphql`
+  {
+    datoCmsBlogLanding {
+      hero {
+        background {
+          gatsbyImageData
+        }
+        title
+      }
+      description
+    }
+    allDatoCmsBlog {
+      edges {
+        node {
+          image {
+            gatsbyImageData
+          }
+          category {
+            id
+            category
+          }
+          title
+          slug
+          excerpt
+        }
+      }
+    }
+  }
+`;
