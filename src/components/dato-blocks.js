155a3db3c036dd33b32c864c4dc513d855faad6d
Emman Toomey <emmantoomey@gmail.com>
2021-09-22 14:51:21 +0100

    added key to mapped iterable

--- src/components/dato-blocks.js
+++ src/components/dato-blocks.js
@@ -47,9 +47,9 @@ const DatoBlocks = ({ blocks }) => {
         </section>
       );
     } else if (block.model.name === "Quote select") {
-      return block.quote.links.map((data) => {
+      return block.quote.links.map((data, i) => {
         const { logo, quote } = data;
-        return <QuoteBlock data={data} companyImage={logo} quote={quote} />;
+        return <QuoteBlock key={'qb_' + i} data={data} companyImage={logo} quote={quote} />;
       });
     } else if (block.model.name === "Values Block") {
       return <Values data={block} />;
