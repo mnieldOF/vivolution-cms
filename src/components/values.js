de8f752c77f1651b45044b44c62b31fa1969a338
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-09-29 10:05:21 +0100

    values block titles fixed

--- src/components/values.js
+++ src/components/values.js
@@ -1,17 +1,15 @@
 import * as React from "react";
 
 const Values = ({ data }) => {
-  console.log("value", data.values.links);
   return (
     <div className="values">
       <div className="content-container">
         <div className="grid">
-          {data.values.links.map((item) => {
-            console.log(item);
+          {data.values.links.map((item, i) => {
             return (
-              <div className="item">
+              <div className="item" key={i}>
                 <img src={item.image.url} alt="" />
-                <h3>{item.title}</h3>
+                <h3 className="title">{item.title}</h3>
                 <p>{item.text}</p>
               </div>
             );
