df34247180a59b45205349b25c36a5421d75d762
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-10-06 22:30:58 +0100

    contact form styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -25,7 +25,7 @@ const sectors = [
 
 const validationSchema = Yup.object().shape({
   sector: Yup.array()
-    .min(1, "Select at least 1 Sector")
+    .min(1, "Select at least 1 option")
     .of(
       Yup.object()
         .shape({ label: Yup.string(), value: Yup.string() })
@@ -91,91 +91,108 @@ const Contact = ({ data }) => {
       <DatoBlocks blocks={blocks} />
       <section className="contact">
         <div className="content-container">
-          <form onSubmit={handleSubmit}>
-            <CustomSelect
-              isMulti
-              options={options}
-              id="department"
-              label={
-                "Which Vivolution services do you want to know more about?"
-              }
-              value={values.department}
-              onChange={(value) => {
-                handleDepartment(value);
-              }}
-            />
-            <CustomSelect
-              isMulti
-              options={sectors}
-              id="sector"
-              label={"What Sector are you from?"}
-              value={values.sector}
-              onChange={(value) => {
-                handleSector(value);
-              }}
-            />
-            {errors.sector ? errors.sector : null}
-            <h2>Please tell us more about yourself and your organisation:</h2>
-            <label htmlFor="name">Name</label>
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={handleChange}
-              value={values.name}
-              placeholder="Your name"
-            />
-            {errors.name ? errors.name : null}
-            <label htmlFor="email">You can reply to me at</label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={handleChange}
-              value={values.email}
-              placeholder="your email address"
-            />
-            {errors.email ? errors.email : null}
-            <label htmlFor="subject">
-              It would be great to have a chat about
-            </label>
-            <input
-              id="subject"
-              name="subject"
-              type="text"
-              onChange={handleChange}
-              value={values.subject}
-              placeholder="let us know how we can help you"
-            />
-            <label htmlFor="website">You can reply to me at</label>
-            <input
-              id="website"
-              name="website"
-              type="text"
-              onChange={handleChange}
-              value={values.website}
-              placeholder="Add you company website here"
-            />
-            <label htmlFor="linkedin">LinkedIn profile</label>
-            <input
-              id="linkedin"
-              name="linkedin"
-              type="text"
-              onChange={handleChange}
-              value={values.linkedin}
-              placeholder="LinkedIn profile"
-            />
-            {errors.linkedin ? errors.linkedin : null}
-            <label htmlFor="help">How can we help?</label>
-            <textarea
-              id="help"
-              name="help"
-              onChange={handleChange}
-              value={values.help}
-              placeholder="How can we help you?"
-            />
-            {errors.help ? errors.help : null}
-            <button type="submit">Submit</button>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="inner">
+              <div className="form-field">
+                <CustomSelect
+                  isMulti
+                  options={options}
+                  id="department"
+                  label={
+                    "Which Vivolution services do you want to know more about?"
+                  }
+                  value={values.department}
+                  onChange={(value) => {
+                    handleDepartment(value);
+                  }}
+                />
+              </div>
+              <div className="form-field">
+                <CustomSelect
+                  isMulti
+                  options={sectors}
+                  id="sector"
+                  label={"What Sector are you from?"}
+                  value={values.sector}
+                  onChange={(value) => {
+                    handleSector(value);
+                  }}
+                />
+                {errors.sector ? <span>{errors.sector}</span> : null}
+              </div>
+              <h3>Please tell us more about yourself and your organisation:</h3>
+              <div className="form-field">
+                <label htmlFor="name">Name</label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  onChange={handleChange}
+                  value={values.name}
+                  placeholder="Your name"
+                />
+                {errors.name ? <span>{errors.name}</span> : null}
+              </div>
+              <div className="form-field">
+                <label htmlFor="email">You can reply to me at</label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  placeholder="Your email address"
+                />
+                {errors.email ? <span>{errors.email}</span> : null}
+              </div>
+              <div className="form-field">
+                <label htmlFor="subject">
+                  It would be great to have a chat about
+                </label>
+                <input
+                  id="subject"
+                  name="subject"
+                  type="text"
+                  onChange={handleChange}
+                  value={values.subject}
+                  placeholder="Let us know how we can help you"
+                />
+              </div>
+              <div className="form-field">
+                <label htmlFor="website">You can reply to me at</label>
+                <input
+                  id="website"
+                  name="website"
+                  type="text"
+                  onChange={handleChange}
+                  value={values.website}
+                  placeholder="Add you company website here"
+                />
+              </div>
+              <div className="form-field">
+                <label htmlFor="linkedin">LinkedIn profile</label>
+                <input
+                  id="linkedin"
+                  name="linkedin"
+                  type="text"
+                  onChange={handleChange}
+                  value={values.linkedin}
+                  placeholder="LinkedIn profile"
+                />
+                {errors.linkedin ? <span>{errors.linkedin}</span> : null}
+              </div>
+              <div className="form-field">
+                <label htmlFor="help">How can we help?</label>
+                <textarea
+                  id="help"
+                  name="help"
+                  onChange={handleChange}
+                  value={values.help}
+                />
+                {errors.help ? <span>{errors.help}</span> : null}
+              </div>
+              <button type="submit">Submit</button>
+            </div>
           </form>
         </div>
       </section>
