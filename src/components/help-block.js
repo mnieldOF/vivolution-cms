7555c3e4e6f51e21f0a3e1ff4297858389e06134
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-10-11 11:36:49 +0100

    moved sector crads into a slider on mobile

--- src/components/help-block.js
+++ src/components/help-block.js
@@ -1,50 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
-import Slider from "react-slick";
 
 import Icon from "./icon";
-import img from "../images/help-bg.png";
-import imgMob from "../images/help-card-bg.png";
 
 const HelpBlock = ({ data }) => {
-  const settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="help-block">
       <div className="content-container column">
         <div className="grid">
-          {data.edges.map(({ node: item }) => (
-            <Link to={`/sectors/${item.slug}`} className="help-card">
+          {data.edges.map(({ node: item, i }) => (
+            <Link key={i} to={`/sectors/${item.slug}`} className="help-card">
               <div className="inner">
                 <div className="content">
                   <div className="icon">
