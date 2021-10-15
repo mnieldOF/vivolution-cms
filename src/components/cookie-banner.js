b7768e7950e122173e4534e52da5fe92e593cd34
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-10-15 12:01:48 +0100

    c

--- src/components/cookie-banner.js
+++ src/components/cookie-banner.js
@@ -9,7 +9,7 @@ const CookieBanner = () => {
     <CookieConsent
       style={{ background: "#fff", color: "#000", fontSize: "14px" }}
     >
-      By clicking “Allow All”, you agree to the storing of cookies on your
+      By clicking “I understand", you agree to the storing of cookies on your
       device to enhance site navigation, analyze site usage, and assist in our
       marketing efforts.
       <Link to={`/policies/privacy-policy`}>Cookie Notice</Link>
