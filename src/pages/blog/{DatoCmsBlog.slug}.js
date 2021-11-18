efe947156667dd638a35b196aadbdcf5c04dd1c0
mnieldOF <68375681+mnieldOF@users.noreply.github.com>
2021-11-18 11:43:08 +0000

    seo hooked up on blogs

--- src/pages/blog/{DatoCmsBlog.slug}.js
+++ src/pages/blog/{DatoCmsBlog.slug}.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { HelmetDatoCms } from "gatsby-source-datocms";
 import { StructuredText } from "react-datocms";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
@@ -10,14 +11,15 @@ const BlogPost = ({ data }) => {
   console.log(data);
   return (
     <Layout>
+      <HelmetDatoCms seo={data.post.seo} />
       <Hero
-        title={data.datoCmsBlog.title}
-        image={data.datoCmsBlog.blocks[0].background.gatsbyImageData}
+        title={data.post.title}
+        image={data.post.blocks[0].background.gatsbyImageData}
       />
-      <Author author={data.datoCmsBlog.author} />
+      <Author author={data.post.author} />
       <div className="content-container blog">
         <StructuredText
-          data={data.datoCmsBlog.content}
+          data={data.post.content}
           renderBlock={({ record }) => {
             if (record.__typename === "DatoCmsImageBlock") {
               const img = getImage(record.image);
@@ -46,8 +48,8 @@ const BlogPost = ({ data }) => {
 export default BlogPost;
 
 export const query = graphql`
-  query($id: String!) {
-    datoCmsBlog(id: { eq: $id }) {
+  query ($id: String!) {
+    post: datoCmsBlog(id: { eq: $id }) {
       id
       blocks {
         background {
@@ -72,6 +74,9 @@ export const query = graphql`
         profileImage {
           gatsbyImageData
         }
+      }
+      seo: seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
       }
     }
   }
