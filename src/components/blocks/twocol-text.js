e2cc34affd977aead30936ff3c27b8c5dc1a6564
Emman <etoomey@vivolution.co.uk>
2026-06-05 17:33:43 +0100

    Render formatted text in the twocol-text components

--- src/components/blocks/twocol-text.js
+++ src/components/blocks/twocol-text.js
@@ -1,5 +1,6 @@
 import "./twocol-text.scss";
 import React from "react";
+import MarkdownText from "./markdown-text";
 
 const TwocolText = ({ data }) => {
   const { textNode, subtitle, title } = data;
@@ -8,14 +9,11 @@ const TwocolText = ({ data }) => {
       <hr className="detail-divider" />
       {subtitle && <p className="detail-section-label">{subtitle}</p>}
       {title && <h2 className="detail-section-headline">{title}</h2>}
-      {textNode && (
-        <div
-          className="detail-prose"
-          dangerouslySetInnerHTML={{
-            __html: textNode.childMarkdownRemark.html,
-          }}
-        />
-      )}
+      <MarkdownText
+        className="detail-prose"
+        html={textNode?.childMarkdownRemark?.html}
+        clean
+      />
     </div>
   );
 };
