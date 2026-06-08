76ac7e32998b8512c8f8ecd2db1cc4d2a091e7e6
Emman <etoomey@vivolution.co.uk>
2026-06-08 10:18:36 +0100

    Update cookie banner styling and fix american spelling

--- src/components/layout/cookie-banner.js
+++ src/components/layout/cookie-banner.js
@@ -6,13 +6,12 @@ import CookieConsent from "react-cookie-consent";
 const CookieBanner = () => {
   return (
     <CookieConsent
-      style={{ background: "#fff", color: "#000", fontSize: "14px" }}
       declineButtonText="I decline"
       enableDeclineButton
       flipButtons
     >
       By clicking `I understand`, you agree to the storing of cookies on your
-      device to enhance site navigation, analyze site usage, and assist in our
+      device to enhance site navigation, analyse site usage, and assist in our
       marketing efforts.
       <Link to={`/policies/privacy-policy`}>Cookie Notice</Link>
     </CookieConsent>
